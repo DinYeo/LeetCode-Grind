8c2aacb4256828648b57f253619c7eeb80a4a5e2
Din <yeodingcheng.19@gmail.com>
2025-05-18 03:33:35 +0800

    Update 217 Contains Duplicate.py

--- 217 Contains Duplicate.py
+++ 217 Contains Duplicate.py
@@ -10,3 +10,13 @@ class Solution:
                 if nums[i] == nums[j]:
                     return True
         return False
+
+# Checking using Hashset
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+        for num in nums:
+            if num in hashset:
+                return True
+            hashset.add(num) # Used to add value to the hashset
+        return False
